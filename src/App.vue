1c0ac29285807e49b5f593a33015c0f79f7380a4
Rônel Dornas <dornas@gpmp.com.br>
2024-11-04 18:47:52 -0300

    fix

--- src/App.vue
+++ src/App.vue
@@ -7,8 +7,8 @@ import { RouterLink, RouterView } from 'vue-router'
     <header>
       <div class="wrapper">
         <nav>
-          <RouterLink to="/">Events</RouterLink> |
-          <RouterLink to="/about">About</RouterLink>
+          <RouterLink :to="{name: 'events'}">Events</RouterLink> |
+          <RouterLink :to="{name: 'about'}">About</RouterLink>
         </nav>
       </div>
     </header>
